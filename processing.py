d72500d6c21a5ff96dccd788c906faa6d4f20ac5
LunexCoding <lunexcoding@vk.com>
2023-04-10 09:41:33 +0700

    Add functionality for data processing
    Added methods `createX`, `createY` splitting data into `X`, `Y`.
    Added `splitData` method for splitting data into training and test sets.
    Added `addPredictColumn` method to add prediction column to dataframe.
    Added `roundColumnValues` method to round column values to the required precision.
    A group of `showDiff` methods has been added that allows you to track the visual accuracy / percentage of correct answers by adding a `diff` column with values:
    *` True` if not matched;
    * `False` if matched.
    As well as getting the number of matches and non-matches.
    Added the ability to display dataframe using the `displayDataFrame` methods.
    Added methods `createX`, `createY` splitting data into `X`, `Y`.
    Added `splitData` method for splitting data into training and test sets.
    Added `addPredictColumn` method to add prediction column to dataframe.
    Added `roundColumnValues` method to round column values to the required precision.
    A group of `showDiff` methods has been added that allows you to track the visual accuracy / percentage of correct answers by adding a `diff` column with values:
    *` True` if not matched;
    * `False` if matched.
    As well as getting the number of matches and non-matches.
    Added the ability to display dataframe using the `displayDataFrame` methods.

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from tabulate import tabulate
 from sklearn.model_selection import train_test_split
 from linearRegression import LinearRegressionModel
 
@@ -6,7 +7,9 @@ from linearRegression import LinearRegressionModel
 class Processing:
     def __init__(self):
         self.__data = None
+        self.__tableFormat = "github"
 
+    # loading data
     def loadData(self, path):
         self.__loadData(path)
         self.__preprocessingData()
@@ -17,12 +20,34 @@ class Processing:
     def __preprocessingData(self):
         self.__data = self.__data.fillna(0)
 
+    # build DataFrame
     def buildDataFrame(self, columns):
-        return self.__buildDataFrame(columns)
+        assert isinstance(columns, list)
+        self.__buildDataFrame(columns)
+        return self.__getDataFrame()
 
     def __buildDataFrame(self, columns):
+        self.__data = self.__data[columns]
+
+    def __getDataFrame(self):
+        return self.__data
+
+    # split data by X, Y
+    def createX(self, columns):
+        assert isinstance(columns, list)
+        return self.__createX(columns)
+
+    def __createX(self, columns):
         return self.__data[columns]
 
+    def createY(self, columns):
+        assert isinstance(columns, list)
+        return self.__createY(columns)
+
+    def __createY(self, columns):
+        return self.__data[columns]
+
+    # create train and test samplings
     def splitData(self, X, Y, randomState=42, testSize=0.2):
         x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=randomState, test_size=testSize)
         return x_train, x_test, y_train, y_test
@@ -32,12 +57,58 @@ class Processing:
         model.loadData(x_train, x_test, y_train, y_test)
         return model
 
-    def clearData(self):
-        self.__clearData()
+    def addPredictColumn(self, columnName, data):
+        self.__data[columnName] = data
+        return self.__getDataFrame()
+
+    # operations with DataFrame
+    def roundColumnValues(self, columnName, digits=0):
+        self.__roundColumnValues(columnName, digits)
+        return self.__getDataFrame()
+
+    def __roundColumnValues(self, columnName, digits):
+        self.__data[columnName] = self.__data[columnName].apply(lambda x: round(x, digits))
+
+    def showDiff(self, dataFrame, Y, predicted, show=False, head=20):
+        self.__showDiff(dataFrame, Y, predicted, show, head)
+
+    def __showDiff(self, dataFrame, nameColumn_Y, predicted, show=False, head=20):
+        dataFrame["diff"] = None
+        dataFrame["diff"] = dataFrame.apply(lambda x: self.__checkDiff(x[nameColumn_Y], x[predicted]), axis=1)
+        if show:
+            if isinstance(head, int):
+                self.displayDataFrame(dataFrame, head)
+                print(self.__countDiff(dataFrame, head))
+            elif head == "full":
+                self.displayDataFrame(dataFrame, head="full")
+                print(self.__countDiff(dataFrame, len(dataFrame)))
+
+    def __checkDiff(self, nameColumn_Y, predicted):
+        return False if predicted == nameColumn_Y else True
+
+    def __countDiff(self, dataFrame, head):
+        return dataFrame["diff"][:head].value_counts()
+
+    def displayDataFrame(self, dataFrame, head=0):
+        if isinstance(head, int):
+            print(tabulate(dataFrame[:head], headers="keys", tablefmt=self.__tableFormat))
+        elif head == "full":
+            print(tabulate(dataFrame, headers="keys", tablefmt=self.__tableFormat))
+
+    @property
+    def tableFormat(self):
+        return self.__tableFormat
+
+    @tableFormat.setter
+    def tableFormat(self, tableFormat):
+        assert isinstance(tableFormat, str)
+        self.__tableFormat = tableFormat
 
     def __clearData(self):
         self.__data = None
 
+    def __del__(self):
+        self.__clearData()
 
 
 
